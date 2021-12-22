1c4e0a8c879994e14efbb1f1d45f2f26471dabbb
Caree Youngman <caree@caree.codes>
2021-12-21 18:03:14 -0600

    added app.use and other handler

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let ceoList = require('./db');
+
 let path = require('path');
 
 const express = require('express');
@@ -16,7 +18,13 @@ app.use(express.static('./public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
 app.get('/', (req, res) => {
+    res.send("hi");
     console.log("home page");
 });
 
